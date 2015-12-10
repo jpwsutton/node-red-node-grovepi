630a7ab23411209cf166d65697a35320ad4012cd
James Sutton <james.sutton@uk.ibm.com>
2015-12-10 16:18:41 +0000

    Getting the simple node working & adding a logo

--- grovepi/grovepi.js
+++ grovepi/grovepi.js
@@ -2,10 +2,15 @@ module.exports = function(RED) {
     function GrovePiNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        setInterval(function(){
+            var msg = {};
+            msg.payload = "Hi";
+            node.send(msg);
+        }, 1000);
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
         });
     }
-    RED.nodes.registerType("grove-pi",LowerCaseNode);
+    RED.nodes.registerType("Grove Button",GrovePiNode);
 }
