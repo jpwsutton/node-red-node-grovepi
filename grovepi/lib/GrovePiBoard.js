7b5bc0addb9917989d877e9e1121901e4911ced3
James Sutton <jpwsutton@gmail.com>
2015-12-14 19:44:34 +0000

    Using advice from Stack Overflow to make GrovePiBoard.js work

--- grovepi/lib/GrovePiBoard.js
+++ grovepi/lib/GrovePiBoard.js
@@ -1,14 +1,18 @@
 var GrovePi = require('node-grovepi').GrovePi;
-var STATE_UNINITIALISED = 0;
-var STATE_INITIALISED = 1;
 
-var state = STATE_UNINITIALISED;
 
 
  var GrovePiBoard = function() {
-   console.log('Initialising GrovePiBoard');
-   this.commands = GrovePi.commands;
-   this.board = new GrovePi.board({
+   console.log('GrovePiBoard.js: Constructor');
+   this.results = this.init.apply(this);
+   console.log('GrovePiBoard.js: Init Complete');
+   console.log(this.results);
+
+ };
+
+ GrovePiBoard.prototype.init = function() {
+   console.log('GrovePiBoard.js: Init Function');
+   var board = new GrovePi.board({
      debug: true,
      onError: function(err){
        console.log('GrovePiBoard.js: Something went wrong');
@@ -27,21 +31,8 @@ var state = STATE_UNINITIALISED;
        }
      }
    });
-
- };
-
- GrovePiBoard.prototype.isInitialised = function(){
-    if(state === STATE_INITIALISED){
-      return true;
-    } else {
-      return false;
-    }
- }
-
- GrovePiBoard.prototype.init = function() {
-   console.log('GrovePiBoard.js: Starting init');
-   console.log(this.board);
-   this.board.init();
+   board.init();
+   return board;
  };
 
  GrovePiBoard.prototype.registerSensor = function(sensorType, pin, repeat, callback){
